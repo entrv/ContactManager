170ee6c42336c382414627ba9c3de97dd6d5f214
entrv <entrvn7@gmail.com>
2015-03-17 16:12:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/entrv/myapplication/a.java
@@ -0,0 +1,12 @@
+package com.example.entrv.myapplication;
+
+import junit.framework.Test;
+
+/**
+ * Created by entrv on 2015-03-12.
+ */
+public class a {
+    public static void main (String[] args) {
+        System.out.println("I rule");
+    }
+}
